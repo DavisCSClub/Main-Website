e7b9b22571e34153a26abf53d8c0ce2bd5770d84
Timothy Tong <tktong@ucdavis.edu>
2015-08-03 13:36:03 -0700

    Issue #35. Officer page is dynamically generated now.

---
+++ src/main/java/org/dcsc/officers/DcscOfficerService.java
@@ -0,0 +1,23 @@
+package org.dcsc.officers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by tktong on 8/3/2015.
+ */
+@Service
+public class DcscOfficerService {
+    @Autowired
+    private DcscOfficerRepository dcscOfficerRepository;
+
+    public List<DcscOfficer> getOfficers(int year) {
+        return dcscOfficerRepository.findDcscOfficersByYear(year);
+    }
+
+    public List<Integer> getDistinctYears() {
+        return dcscOfficerRepository.findDistinctYears();
+    }
+}
