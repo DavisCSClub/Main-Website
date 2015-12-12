331b22220681fbef9cb8d5db7d9a74f6950ba26e
Timothy Tong <tktong@ucdavis.edu>
2015-12-11 23:54:16 -0800

    #108

---
+++ utilities/src/main/java/org/dcsc/utilities/converter/LocalDateTimeConverter.java
@@ -0,0 +1,23 @@
+package org.dcsc.utilities.converter;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Converter(autoApply = true)
+public class LocalDateTimeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+    @Override
+    public Timestamp convertToDatabaseColumn(LocalDateTime attribute) {
+        return Timestamp.valueOf(attribute);
+    }
+
+    @Override
+    public LocalDateTime convertToEntityAttribute(Timestamp dbData) {
+        if (dbData == null) {
+            return null;
+        }
+
+        return dbData.toLocalDateTime();
+    }
+}
