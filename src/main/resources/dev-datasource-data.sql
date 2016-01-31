4d48b473e91803396000dbe7857d2e1688688c1f
Timothy Tong <tktong418@gmail.com>
2016-01-30 16:02:00 -0800

    #134 Added back the property files from the resources

---
+++ src/main/resources/dev-datasource-data.sql
@@ -0,0 +1,57 @@
+INSERT INTO dcsc_accounts.dcsc_roles VALUES (1, 'SUPER');
+INSERT INTO dcsc_accounts.dcsc_roles VALUES (2, 'BASIC');
+
+INSERT INTO dcsc_accounts.dcsc_class VALUES (1, 'CAROUSEL');
+INSERT INTO dcsc_accounts.dcsc_class VALUES (2, 'EVENT');
+INSERT INTO dcsc_accounts.dcsc_class VALUES (3, 'USER');
+
+INSERT INTO dcsc_accounts.dcsc_role_privileges VALUES (1, 1, 15);
+INSERT INTO dcsc_accounts.dcsc_role_privileges VALUES (1, 2, 15);
+INSERT INTO dcsc_accounts.dcsc_role_privileges VALUES (1, 3, 15);
+
+INSERT INTO dcsc_accounts.dcsc_role_privileges VALUES (2, 1, 1);
+INSERT INTO dcsc_accounts.dcsc_role_privileges VALUES (2, 2, 1);
+INSERT INTO dcsc_accounts.dcsc_role_privileges VALUES (2, 3, 1);
+
+INSERT INTO dcsc_accounts.dcsc_profiles VALUES (1, 'Root', 'johnDoe@nowhere.org', 'President', 'Sample Profile');
+INSERT INTO dcsc_accounts.dcsc_profiles VALUES (2, 'John Doe', 'johnDoe@nowhere.org', 'Title', 'Sample Profile');
+
+-- local development password for these users is "password"
+INSERT INTO dcsc_accounts.dcsc_users VALUES (1, 'dcsc', '$2a$10$LLExsv.PneZ4I.iT6C3ZyO45g4xRYLDZmtIkaYj.qRkd2bwrCTnDC', TRUE,FALSE,1,1 );
+INSERT INTO dcsc_accounts.dcsc_users VALUES (2, 'john.doe', '$2a$10$LLExsv.PneZ4I.iT6C3ZyO45g4xRYLDZmtIkaYj.qRkd2bwrCTnDC', TRUE,FALSE,2,2);
+
+INSERT INTO dcsc_accounts.dcsc_groups VALUES (1, 'My Group');
+INSERT INTO dcsc_accounts.dcsc_groups VALUES (2, 'Tutoring Committee');
+
+INSERT INTO dcsc_accounts.dcsc_user_groups VALUES (1,1, TRUE);
+INSERT INTO dcsc_accounts.dcsc_user_groups VALUES (1,2, TRUE);
+INSERT INTO dcsc_accounts.dcsc_user_groups VALUES (2,2, FALSE);
+
+INSERT INTO public.dcsc_events VALUES (1, 'Sample Event', 'Sample Description', 'tag', '2016-01-02', '11:30:00', '15:46:00', NULL, 'somewhere',TRUE, NULL, NULL, NULL,NULL);
+
+INSERT INTO public.dcsc_images VALUES (1, 'Sample Image', NULL, '/static/img/placeholder/placeholder-1920-700.jpg', 'Sample Image Description');
+
+INSERT INTO public.dcsc_carousel VALUES (1, 'Sample Caption', 1, 1);
+
+INSERT INTO dcsc_tutoring.dcsc_tutors VALUES (1,1,TRUE);
+INSERT INTO dcsc_tutoring.dcsc_tutors VALUES (2,2,TRUE);
+
+INSERT INTO dcsc_tutoring.dcsc_courses VALUES (1, 'ECS10');
+INSERT INTO dcsc_tutoring.dcsc_courses VALUES (2, 'ECS12');
+INSERT INTO dcsc_tutoring.dcsc_courses VALUES (3, 'ECS15');
+INSERT INTO dcsc_tutoring.dcsc_courses VALUES (4, 'ECS20');
+INSERT INTO dcsc_tutoring.dcsc_courses VALUES (5, 'ECS30');
+
+INSERT INTO dcsc_time.dcsc_academic_term VALUES (1, 'FQ', 2015, '2015-09-21', '2015-12-11');
+INSERT INTO dcsc_time.dcsc_academic_term VALUES (2, 'WQ', 2016, '2016-01-04', '2016-03-19');
+
+INSERT INTO dcsc_tutoring.dcsc_tutoring VALUES (1, 1, 1);
+INSERT INTO dcsc_tutoring.dcsc_tutoring VALUES (1, 2, 1);
+INSERT INTO dcsc_tutoring.dcsc_tutoring VALUES (2, 2, 1);
+INSERT INTO dcsc_tutoring.dcsc_tutoring VALUES (2, 1, 2);
+INSERT INTO dcsc_tutoring.dcsc_tutoring VALUES (2, 2, 2);
+INSERT INTO dcsc_tutoring.dcsc_tutoring VALUES (2, 3, 2);
+
+INSERT INTO dcsc_tutoring.dcsc_office_hours VALUES (1, '2016-01-16 02:30:00+00', '2016-01-16 04:30:00+00', 2, NULL);
+
+INSERT INTO dcsc_tutoring.has_office_hours VALUES (2, 1);
---
+++ src/main/resources/dev-datasource-schema.sql
@@ -0,0 +1,138 @@
+CREATE SCHEMA IF NOT EXISTS administration;
+CREATE SCHEMA IF NOT EXISTS dcsc_accounts;
+CREATE SCHEMA IF NOT EXISTS dcsc_tutoring;
+CREATE SCHEMA IF NOT EXISTS dcsc_time;
+
+CREATE TABLE IF NOT EXISTS dcsc_accounts.dcsc_roles (
+  id   INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS dcsc_accounts.dcsc_class (
+  id         INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  class_name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS dcsc_accounts.dcsc_role_privileges (
+  role_id       INT NOT NULL,
+  permission_id INT NOT NULL,
+  access_level  INT NOT NULL,
+  CONSTRAINT role_fk FOREIGN KEY (role_id) REFERENCES dcsc_accounts.dcsc_roles (id),
+  CONSTRAINT permission_fk FOREIGN KEY (permission_id) REFERENCES dcsc_accounts.dcsc_class (id)
+);
+
+CREATE TABLE IF NOT EXISTS dcsc_accounts.dcsc_profiles (
+  id          INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name        VARCHAR(255),
+  email       VARCHAR(255),
+  title       VARCHAR(255),
+  description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS dcsc_accounts.dcsc_users (
+  id         INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  username   VARCHAR(255),
+  password   VARCHAR(60),
+  is_enabled BOOLEAN,
+  is_locked  BOOLEAN,
+  role_id    INT,
+  profile_id INT,
+  CONSTRAINT user_role_fk FOREIGN KEY (role_id) REFERENCES dcsc_accounts.dcsc_roles (id),
+  CONSTRAINT user_profile_fk FOREIGN KEY (profile_id) REFERENCES dcsc_accounts.dcsc_profiles (id)
+);
+
+CREATE TABLE IF NOT EXISTS dcsc_accounts.dcsc_groups (
+  id   INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS dcsc_accounts.dcsc_user_groups (
+  user_id  INT     NOT NULL,
+  group_id INT     NOT NULL,
+  is_admin BOOLEAN NOT NULL,
+  CONSTRAINT group_user_id_fk FOREIGN KEY (user_id) REFERENCES dcsc_accounts.dcsc_users (id),
+  CONSTRAINT group_group_id_fk FOREIGN KEY (group_id) REFERENCES dcsc_accounts.dcsc_groups (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.dcsc_images (
+  id            INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name          TEXT,
+  absolute_path TEXT,
+  relative_path TEXT,
+  description   TEXT
+);
+
+CREATE TABLE IF NOT EXISTS public.dcsc_events (
+  id              INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name            VARCHAR(70),
+  description     TEXT,
+  tag             VARCHAR(30),
+  date            DATE,
+  starttime       TIME,
+  endtime         TIME,
+  image           VARCHAR(100),
+  location        VARCHAR(50),
+  published       BOOLEAN,
+  thumbnail_id    INT,
+  banner_id       INT,
+  start_date_time TIMESTAMP,
+  end_date_time   TIMESTAMP,
+  CONSTRAINT thumbnail_fk FOREIGN KEY (thumbnail_id) REFERENCES public.dcsc_images (id),
+  CONSTRAINT banner_fk FOREIGN KEY (banner_id) REFERENCES public.dcsc_images (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.dcsc_carousel (
+  id         INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  caption    VARCHAR(255),
+  image_id   INT,
+  precedence INT,
+  CONSTRAINT image_fk FOREIGN KEY (image_id) REFERENCES public.dcsc_images (id)
+);
+
+CREATE TABLE IF NOT EXISTS dcsc_tutoring.dcsc_tutors (
+  id        INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  common_id INT                            NOT NULL,
+  is_active BOOLEAN,
+  CONSTRAINT dcsc_tutor_user_profile_fk FOREIGN KEY (common_id) REFERENCES dcsc_accounts.dcsc_users (id),
+);
+
+CREATE TABLE IF NOT EXISTS dcsc_tutoring.dcsc_courses (
+  id   INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  code VARCHAR(10)
+);
+
+CREATE TABLE IF NOT EXISTS dcsc_time.dcsc_academic_term (
+  id         INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  code       VARCHAR(3),
+  year       INT,
+  start_date DATE,
+  end_date   DATE
+);
+
+CREATE TABLE IF NOT EXISTS dcsc_tutoring.dcsc_tutoring (
+  tutor_id  INT,
+  course_id INT,
+  term_id   INT,
+  PRIMARY KEY (tutor_id, course_id, term_id),
+  CONSTRAINT dcsc_tutoring_tutor_fk FOREIGN KEY (tutor_id) REFERENCES dcsc_tutoring.dcsc_tutors (id),
+  CONSTRAINT dcsc_tutoring_course_fk FOREIGN KEY (course_id) REFERENCES dcsc_tutoring.dcsc_courses (id),
+  CONSTRAINT dcsc_tutoring_quarter_fk FOREIGN KEY (term_id) REFERENCES dcsc_time.dcsc_academic_term (id)
+);
+
+CREATE TABLE IF NOT EXISTS dcsc_tutoring.dcsc_office_hours (
+  id              INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  start_timestamp TIMESTAMP                      NOT NULL,
+  end_timestamp   TIMESTAMP                      NOT NULL,
+  term_id         INT                            NOT NULL,
+  child_id        INT,
+  CONSTRAINT dcsc_oh_term_fk FOREIGN KEY (term_id) REFERENCES dcsc_time.dcsc_academic_term (id),
+  CONSTRAINT dcsc_oh_child_fk FOREIGN KEY (child_id) REFERENCES dcsc_tutoring.dcsc_office_hours (id),
+);
+
+CREATE TABLE IF NOT EXISTS dcsc_tutoring.has_office_hours (
+  tutor_id       INT,
+  office_hour_id INT,
+  PRIMARY KEY (tutor_id, office_hour_id),
+  CONSTRAINT dcsc_oh_tutor_fk FOREIGN KEY (tutor_id) REFERENCES dcsc_tutoring.dcsc_tutors (id),
+  CONSTRAINT dcsc_oh_office_hour_fk FOREIGN KEY (office_hour_id) REFERENCES dcsc_tutoring.dcsc_office_hours (id)
+);
