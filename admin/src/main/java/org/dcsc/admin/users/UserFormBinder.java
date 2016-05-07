f22bd5104a01c5ff68faac4faf3c8ecc948a491b
Timothy Tong <tktong418@gmail.com>
2016-05-07 00:12:26 -0700

    #173 Improved user form binder

--- admin/src/main/java/org/dcsc/admin/users/UserFormBinder.java
+++ admin/src/main/java/org/dcsc/admin/users/UserFormBinder.java
@@ -5,37 +5,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Component
 public class UserFormBinder {
     public void bindToUser(DcscUser user, Map<String, Object> userForm) throws Exception {
-        for (Map.Entry<String, Object> entry : userForm.entrySet()) {
-            String key = entry.getKey();
-            Object value = entry.getValue();
+        Optional.ofNullable((String) userForm.get("user_form_description"))
+                .ifPresent(description -> user.getUserProfile().setDescription(description));
 
-            if (isProfileDescription(key)) {
-                user.getUserProfile().setDescription((String) value);
-            } else if (isFormPassword(key) && passwordMatches((String) value, (String) userForm.get("user_form_confirm_password"))) {
-                user.setPassword(new BCryptPasswordEncoder().encode((String) value));
-            } else if (isFormEmail(key)) {
-                user.getUserProfile().setEmail((String) value);
-            }
-        }
-    }
+        Optional.ofNullable((String) userForm.get("user_form_password"))
+                .filter(password -> !password.isEmpty())
+                .filter(password -> password.equals(userForm.get("user_form_confirm_password")))
+                .ifPresent(password -> user.setPassword(new BCryptPasswordEncoder().encode(password)));
 
-    private boolean isProfileDescription(String key) {
-        return "user_form_description".equals(key);
-    }
-
-    private boolean isFormPassword(String key) {
-        return "user_form_password".equals(key);
-    }
-
-    private boolean passwordMatches(String password, String confirmPassword) {
-        return password.matches(confirmPassword);
-    }
-
-    private boolean isFormEmail(String key) {
-        return "user_form_email".equals(key);
+        Optional.ofNullable((String) userForm.get("user_form_email"))
+                .ifPresent(email -> user.getUserProfile().setEmail(email));
     }
 }
