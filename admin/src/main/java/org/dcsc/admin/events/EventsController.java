972ca48f4b5aa9270ace3354a457597fbc007181
Timothy Tong <tktong418@gmail.com>
2016-05-19 19:02:34 -0700

    #182 Convert controller methods to Spring 4.3 style and added exception handler for non supported http methods

--- admin/src/main/java/org/dcsc/admin/events/EventsController.java
+++ admin/src/main/java/org/dcsc/admin/events/EventsController.java
@@ -10,6 +10,7 @@ import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
 
@@ -24,27 +25,21 @@ public class EventsController {
     private EventResourceAssembler resourceAssembler;
 
     @PreAuthorize("hasPermission('event', 'read')")
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public PagedResources<Event> getEvents(Pageable pageable, PagedResourcesAssembler assembler) {
         return assembler.toResource(eventService.getEvents(pageable), resourceAssembler);
     }
 
     @PreAuthorize("hasPermission('event', 'create')")
     @ResponseStatus(HttpStatus.CREATED)
-    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public void createEvent(@RequestBody Event event) {
         event.setDescription(HtmlUtils.htmlEscape(event.getDescription()));
         eventService.saveEvent(event);
     }
 
     @PreAuthorize("hasPermission('event', 'read')")
-    @RequestMapping(value = "/template", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Event getEventTemplate() {
-        return new Event();
-    }
-
-    @PreAuthorize("hasPermission('event', 'read')")
-    @RequestMapping(value = "/{eventId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{eventId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Resource<Event> getEvent(@PathVariable("eventId") int eventId) {
         Event event = eventService.getEventById(eventId).get();
         event.setDescription(HtmlUtils.htmlUnescape(event.getDescription()));
@@ -68,5 +63,17 @@ public class EventsController {
     @RequestMapping(value = "/{eventId}", method = RequestMethod.DELETE)
     public void deleteEvent(@PathVariable("eventId") int eventId) {
         eventService.deleteEventById(eventId);
+    }
+
+    @PreAuthorize("hasPermission('event', 'read')")
+    @GetMapping(value = "/template", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Event getEventTemplate() {
+        return new Event();
+    }
+
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public void nonSupportedHttpMethods(HttpRequestMethodNotSupportedException exception) {
+        // nop
     }
 }
